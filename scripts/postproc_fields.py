fixing derivatives

scripts/postproc_fields.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sympy as sp
 import sys, os, sympy, shutil, math
+import site
+site.addsitedir('../src')
+
 # import xmltodict
 # import pickle
 import json
@@ -13,10 +16,11 @@ import pandas as pd
 import dolfin
 
 import os.path
+import argparse
 
-print('postproc')
-from matplotlib.ticker import ScalarFormatter
-from matplotlib.ticker import FormatStrFormatter
+from postprocess import load_data
+
+print('postproc fields')
 
 import mpi4py
 
@@ -24,34 +28,17 @@ comm = mpi4py.MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--experiment", "-e", type=str, default=None, help='folder containing xdmf data')
+parser.add_argument("--stride", "-s", type=int, default=10, help='stride of temporal sampling')
+parser.add_argument("--xres", "-nx", type=int, default=100, help='spatial resolution (points)')
+args, unknown = parser.parse_known_args()
+print(args)
+if args.experiment is not None:
+    rootdir = args.experiment
+print(rootdir)
+stride = args.stride
 
-def load_data(rootdir):
-	with open(rootdir + '/parameters.pkl', 'r') as f:
-		params = json.load(f)
-
-	with open(rootdir + '/time_data.json', 'r') as f:
-		data = json.load(f)
-		dataf = pd.DataFrame(data).sort_values('load')
-
-	if os.path.isfile(rootdir + '/signature.md5'):
-#         print('sig file found')
-		with open(rootdir + '/signature.md5', 'r') as f:
-			signature = f.read()
-	else:
-		print('no sig file found')
-		signature = hashlib.md5(str(params).encode('utf-8')).hexdigest()
-
-	return params, dataf, signature 
-
-
-
-# rootdir = '../output/traction-shortbar-dc94bca726c96b13a96912e21834b02a'
-# rootdir = 'test-b73266c3fa7937d06a1b4b6ea4f01dca'
-# outdir = 'test-'
-
-# rootdir = '/Users/kumiori/Documents/WIP/paper_stability_code/output/traction-047c3f35cb3e998158cc243504ffd36a'
-# rootdir = '/Users/kumiori/Documents/WIP/paper_stability_code/output/traction-9c1720ed3a8527baf2c8825d8a7c6ae6'
-rootdir = '/Users/kumiori/Documents/WIP/paper_stability_code/output/traction-longbar-1f11d5cf5b3ae17a5062991bf39faeb5'
 
 params, data, signature = load_data(rootdir)
 
@@ -61,8 +48,17 @@ V_alpha = dolfin.FunctionSpace(mesh, "CG", 1)
 load_min = params['time_stepping']['load_min']
 load_max = params['time_stepping']['load_max']
 nsteps = params['time_stepping']['nsteps']
-print(nsteps)
+Lx = params['geometry']['Lx']
+
+onedim = True
+if not onedim:
+	Ly = params['geometry']['Ly']
+
+xs = np.linspace(-Lx/2, Lx/2, args.xres)
+y0 = 0.
+
 load_steps = np.linspace(load_min, load_max, nsteps)
+
 beta0 = dolfin.Function(V_alpha)
 alpha = dolfin.Function(V_alpha)
 alpha_old = dolfin.Function(V_alpha)
@@ -70,40 +66,74 @@ alpha_bif = dolfin.Function(V_alpha)
 
 maxmodes = 2
 
+alpha = dolfin.Function(V_alpha)
+alphas = []
+
+file_postproc = dolfin.XDMFFile(os.path.join(rootdir, "output_postproc.xdmf"))
+file_postproc.parameters["functions_share_mesh"] = True
+file_postproc.parameters["flush_output"] = True
+
+# stride = 10
+for (step, load) in enumerate(load_steps):
+	if not step % stride:
+		with file_postproc as file:
+			print('DEBUG: reading file', os.path.join(rootdir, "output_postproc.xdmf"))
+			print('DEBUG: reading step', step)
+			file_postproc.read_checkpoint(alpha, 'alpha-{}'.format(step), 0)
+			# import pdb; pdb.set_trace()
+			if onedim:
+				alphav = [alpha(x) for x in xs]
+			else:
+				alphav = [alpha(x, y0) for x in xs]
+			alphas.append(alphav)
+
+np.save(os.path.join(rootdir, "alpha"), alphas,
+	allow_pickle=True, fix_imports=True)
+print('Saved {}'.format(os.path.join(rootdir, 'alpha.npy')))
+data = np.load(os.path.join(rootdir, "alpha.npy".format(0)))
+
 
 perturbations = []
 
-Lx = params['geometry']['Lx']
-Ly = params['geometry']['Ly']
-xs = np.linspace(-Lx/2, Lx/2, 100)
-h0 = 0.
 
+betan = dolfin.Function(V_alpha)
+maxmodes = 1
+fields = []
+h0=0
+fields = [beta0, alpha_old, alpha_bif, alpha]
 try:
 	with dolfin.XDMFFile(os.path.join(rootdir, "bifurcation_postproc.xdmf")) as file:
+	# with dolfin.XDMFFile(os.path.join(rootdir, "postproc.xdmf")) as file:
 		file.read_checkpoint(beta0, 'beta0')
 		file.read_checkpoint(alpha, 'alpha')
 		file.read_checkpoint(alpha_old, 'alpha-old')
 		file.read_checkpoint(alpha_bif, 'alpha-bif')
 
-	for field, name in zip(fields, ['beta0', 'alpha_old', 'alpha_bif', 'alpha']):
-		fieldv = [field(x, h0) for x in xs]
+	for field, name in zip(fields, ['beta0', 'alpha_old', 'alpha_bif', 'alpha0']):
+		if onedim:
+			fieldv = [field(x) for x in xs]
+		else:
+			fieldv = [field(x, h0) for x in xs]
+		print('saving {}'.format(name))
 		np.save(os.path.join(rootdir, name), fieldv,
 			allow_pickle=True, fix_imports=True)
 
-	fields = [beta0, alpha_old, alpha_bif, alpha]
-		# for n in range(maxmodes):
-		# 	modename = 'beta-%d'%n
-		# 	print(modename)
-		# 	file.read_checkpoint(betan, modename)
+	for n in range(maxmodes):
+		modename = 'beta-%d'%n
+		print(modename)
+		file.read_checkpoint(betan, modename)
 
-		# 	perturbations.append(betan)
-		# 	betanv = [betan(x, h0) for x in xs]
-		# 	np.save(os.path.join(rootdir, "beta-{}".format(n)), betanv,
-		# 		allow_pickle=True, fix_imports=True)
+		perturbations.append(betan)
+		if onedim:
+			betanv = [betan(x) for x in xs]
+		else:
+			betanv = [betan(x, h0) for x in xs]
+		np.save(os.path.join(rootdir, "beta-{}".format(n)), betanv,
+			allow_pickle=True, fix_imports=True)
 
-
-except:
-	print('no bifurcation data found')
+except Exception as e: 
+	print(e)
+	# print('no bifurcation data found')
 # else:
 # 	pass
 # finally:
@@ -114,33 +144,14 @@ except:
 # nmodes = len(perturbations)
 
 
-alpha = dolfin.Function(V_alpha)
-alphas = []
-for (step, load) in enumerate(load_steps):
-	if not step % 10:
-		with dolfin.XDMFFile(os.path.join(rootdir, "output_postproc.xdmf")) as file:
-			print('reading step', step)
-			file.read_checkpoint(alpha, 'alpha-{}'.format(step), 0)
-			# dolfin.plot(alpha, vmin=0, vmax=1)
-			# plt.savefig('_alpha_{}.pdf'.format(step))
-			alphav = [alpha(x, h0) for x in xs]
-			# print(alphav)
-			alphas.append(alphav)
-
-np.save(os.path.join(rootdir, "alpha"), alphas,
-	allow_pickle=True, fix_imports=True)
-
-data = np.load(os.path.join(rootdir, "alpha.npy".format(0)))
-
 # ------------
 
-import pdb; pdb.set_trace()
 
-z = np.polyfit(htest, en, m)
-p = np.poly1d(z)
+# z = np.polyfit(htest, en, m)
+# p = np.poly1d(z)
 
-3
-np.linspace(hmin, hmax, 4)
+# 3
+# np.linspace(hmin, hmax, 4)
 
 
 

scripts/traction_1d.py
@@ -99,7 +99,7 @@ numerical_parameters = {"alt_min": alt_min_parameters,
                       "time_stepping": timestepping_parameters}
 
 versions = get_versions()
-versions.update({'filename': __file__})
+versions.update({'filename': os.path.basename(__file__)})
 parameters = {"alt_min": alt_min_parameters,
                 "stability": stability_parameters,
                 "time_stepping": timestepping_parameters,
@@ -193,7 +193,7 @@ def traction_1d(
         configuration = deepcopy(parameters)
         configuration['time_stepping'].pop('outdir')
         str(configuration).replace("\'True\'", "True").replace("\'False\'", "False")
-        rerun_cmd = 'python3 {} --config="{}"'.format(__file__, configuration)
+        rerun_cmd = 'python3 {} --config="{}"'.format(os.path.basename(__file__), configuration)
         f.write(rerun_cmd)
 
     with open(os.path.join(outdir, 'parameters.pkl'), 'w') as f:
@@ -204,17 +204,12 @@ def traction_1d(
 
 
     print('experiment = {}'.format(os.path.join('~/Documents/WIP/paper_stability_code', outdir)))
-    # geom = mshr.Rectangle(dolfin.Point(-Lx/2., -Ly/2.), dolfin.Point(Lx/2., Ly/2.))
-    # mesh = mshr.generate_mesh(geom,  int(float(n * Lx / ell)))
     mesh = dolfin.IntervalMesh(int(float(n * Lx / ell)), -Lx/2., Lx/2.)
     meshf = dolfin.File(os.path.join(outdir, "mesh.xml"))
     meshf << mesh
 
     left = dolfin.CompiledSubDomain("near(x[0], -Lx/2.)", Lx=Lx)
     right = dolfin.CompiledSubDomain("near(x[0], Lx/2.)", Lx=Lx)
-    # bottom = dolfin.CompiledSubDomain("near(x[1],-Ly/2.)", Ly=Ly)
-    # top = dolfin.CompiledSubDomain("near(x[1],Ly/2.)", Ly=Ly)
-    # left_bottom_pt = dolfin.CompiledSubDomain("near(x[0],-Lx/2.) && near(x[1],-Ly/2.)", Lx=Lx, Ly=Ly)
 
     mf = dolfin.MeshFunction("size_t", mesh, 1, 0)
     right.mark(mf, 1)
@@ -298,8 +293,8 @@ def traction_1d(
     # stability = StabilitySolver(mesh, energy, [u, alpha], [bcs_u, bcs_alpha], z, parameters = parameters['stability'])
 
     # Time iterations
-    # load_steps = np.linspace(load_min, load_max, parameters['time_stepping']['nsteps'])
-    load_steps = np.logspace(np.log10(load_min), np.log10(load_max), parameters['time_stepping']['nsteps'])
+    load_steps = np.linspace(load_min, load_max, parameters['time_stepping']['nsteps'])
+    # load_steps = np.logspace(np.log10(load_min), np.log10(load_max), parameters['time_stepping']['nsteps'])
 
     if loads:
         load_steps = loads
@@ -324,6 +319,7 @@ def traction_1d(
     bifurcation_loads = []
     time_data_pd = []
     for it, load in enumerate(load_steps):
+        # import pdb; pdb.set_trace()
         ut.t = load
         alpha_old.assign(alpha)
 
@@ -480,7 +476,7 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--config", required=False,
                         help="JSON configuration string for this experiment")
     parser.add_argument("--ell", type=float, default=0.1)
-    parser.add_argument("--load_max", type=float, default=2.0)
+    parser.add_argument("--load_max", type=float, default=3.0)
     parser.add_argument("--load_min", type=float, default=0.0)
     parser.add_argument("--E", type=float, default=1)
     parser.add_argument("--sigma_D0", type=float, default=1)
@@ -578,20 +574,15 @@ if __name__ == "__main__":
     tc = np.sqrt(2*w1/2*params['material']['E'])
     ell = params['material']['ell']
     lab = '\\ell={}, E={}, \\sigma_D = {}'.format(params['material']['ell'], params['material']['E'],params['material']['sigma_D0'])
+    params['geometry']['Ly'] =1
     fig1, ax1 =pp.plot_energy(params, data, tc)
-    visuals.setspines2()
+    # visuals.setspines2()
     mu = params['material']['E']/2.
     # elast_en = [1./2.*2.*mu*eps**2 for eps in data['load']]
     elast_en = [1./2.*params['material']['E']*eps**2 for eps in data['load']]
     plt.plot(data['load'], elast_en, c='k')
     ax1.axvline(pp.t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
     ax1.axvline(pp.t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
-
-
-
-    ax1.get_yaxis().set_major_formatter(ScalarFormatter())
-    ax1.ticklabel_format(axis='both', style='plain', useOffset=True)
-
 
     plt.title('${}$'.format(lab))
 
@@ -601,15 +592,11 @@ if __name__ == "__main__":
     ax1.axvline(pp.t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
     ax1.axvline(pp.t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
 
-    # ax1.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-    ax1.get_yaxis().set_major_formatter(ScalarFormatter())
-    ax1.yaxis.set_major_formatter(FormatStrFormatter('%0.0e'))
-    # ax1.ticklabel_format(axis='y', style='sci', useOffset=True)
     plt.plot(np.linspace(1, params['time_stepping']['load_max'], 30),
              [1- (t/params['material']['sigma_D0']/params['material']['E'])**(2/(1-2)) for t in np.linspace(1, params['time_stepping']['load_max'], 30)],
             c='k', lw=.5)
     plt.title('${}$'.format(lab))
-    visuals.setspines2()
+    # visuals.setspines2()
     ax1.set_ylim(-1., .2)
 
     # fig1.savefig("/Users/kumiori/Documents/WIP/paper_stability/fig/energy-traction-{}.pdf".format(signature), bbox_inches='tight')
@@ -621,5 +608,5 @@ if __name__ == "__main__":
     mesh = dolfin.Mesh(comm, os.path.join(experiment, 'mesh.xml'))
     fig = plt.figure()
     dolfin.plot(mesh)
-    visuals.setspines2()
+    # visuals.setspines2()
     fig.savefig(os.path.join(location, "mesh.pdf"), bbox_inches='tight')

scripts/traction_continuation.py
@@ -197,7 +197,7 @@ def traction_test(
         configuration = deepcopy(parameters)
         configuration['time_stepping'].pop('outdir')
         str(configuration).replace("\'True\'", "True").replace("\'False\'", "False")
-        rerun_cmd = 'python3 traction.py --config="{}"'.format(configuration)
+        rerun_cmd = 'python3 {} --config="{}"'.format(__file__, configuration)
         f.write(rerun_cmd)
 
     with open(os.path.join(outdir, 'parameters.pkl'), 'w') as f:
@@ -256,6 +256,12 @@ def traction_test(
     file_eig = dolfin.XDMFFile(os.path.join(outdir, "modes.xdmf"))
     file_eig.parameters["functions_share_mesh"] = True
     file_eig.parameters["flush_output"] = True
+    file_postproc = dolfin.XDMFFile(os.path.join(outdir, "output_postproc.xdmf"))
+    file_postproc.parameters["functions_share_mesh"] = True
+    file_postproc.parameters["flush_output"] = True
+    file_bif = dolfin.XDMFFile(os.path.join(outdir, "bifurcation_postproc.xdmf"))
+    file_bif.parameters["functions_share_mesh"] = True
+    file_bif.parameters["flush_output"] = True
 
     # Problem definition
     model = DamageElasticityModel(state, E0, nu, ell, sigma_D0)
@@ -379,6 +385,11 @@ def traction_test(
         time_data_i["# neg ev"] = stability.negev
         # import pdb; pdb.set_trace()
 
+        _sigma = model.stress(model.eps(u), alpha)
+        e1 = dolfin.Constant([1, 0])
+        _snn = dolfin.dot(dolfin.dot(_sigma, e1), e1)
+        time_data_i["sigma"] = 1/Ly * dolfin.assemble(_snn*model.ds(1))
+
         time_data_i["S(alpha)"] = dolfin.assemble(1./(model.a(alpha))*model.dx)
         time_data_i["A(alpha)"] = dolfin.assemble((model.a(alpha))*model.dx)
         time_data_i["avg_alpha"] = dolfin.assemble(alpha*model.dx)
@@ -393,19 +404,25 @@ def traction_test(
         time_data_pd = pd.DataFrame(time_data)
 
         if np.mod(it, savelag) == 0:
-            with file_out as f:
-                f.write(alpha, load)
-                f.write(u, load)
-            with dolfin.XDMFFile(os.path.join(outdir, "output_postproc.xdmf")) as f:
-                f.write_checkpoint(alpha, "alpha-{}".format(it), 0, append = True)
+            with file_out as file:
+                file.write(alpha, load)
+                file.write(u, load)
+
+            with file_postproc as file:
+                file.write_checkpoint(alpha, "alpha-{}".format(it), 0, append = True)
                 print('DEBUG: written step ', it)
+                file.read_checkpoint(alpha, 'alpha-{}'.format(it), 0)
+                print('DEBUG: read step {}, load {}'.format(it, load))
+                print('DEBUG: file {}'.format(os.path.join(outdir, "output_postproc.xdmf")))
+                # import pdb; pdb.set_trace()
 
         if save_current_bifurcation:
             # modes = np.where(stability.eigs < 0)[0]
             time_data_i['h_opt'] = h_opt
             time_data_i['max_h'] = hmax
             time_data_i['min_h'] = hmin
-            with dolfin.XDMFFile(os.path.join(outdir, "bifurcation_postproc.xdmf")) as file:
+
+            with file_bif as file:
                 # leneigs = len(modes)
                 # maxmodes = min(3, leneigs)
                 beta0v = dolfin.project(stability.perturbation_beta, V_alpha)
@@ -416,15 +433,18 @@ def traction_test(
                 file.write_checkpoint(alpha, 'alpha', 0, append=True)
 
                 np.save(os.path.join(outdir, 'energy_perturbations'), energy_perturbations, allow_pickle=True, fix_imports=True)
-                # import pdb; pdb.set_trace()
 
-                # maxmodes = 2
-                # for n in range(maxmodes):
-                #     mode = dolfin.project(linsearch[n]['beta_n'], V_alpha)
-                #     modename = 'beta-%d'%n
-                #     print(modename)
-                #     file.write_checkpoint(mode, modename, 0, append=True)
-                #     import pdb; pdb.set_trace()
+            with file_eig as file:
+                _v = dolfin.project(dolfin.Constant(h_opt)*perturbation_v, V_u)
+                _beta = dolfin.project(dolfin.Constant(h_opt)*perturbation_beta, V_alpha)
+                _v.rename('perturbation displacement', 'perturbation displacement')
+                _beta.rename('perturbation damage', 'perturbation damage')
+                # import pdb; pdb.set_trace()
+                file.write(_v, load)
+                file.write(_beta, load)
+
+            save_current_bifurcation = False
+
 
         # if np.mod(it, 10) == 0:
         #     alphatest = dolfin.Function(V_alpha)
@@ -432,7 +452,7 @@ def traction_test(
         #         f.read_checkpoint(alphatest, "alpha-{}".format(it), 0)
         #         print('DEBUG: read step ', it)
 
-            time_data_pd.to_json(os.path.join(outdir, "time_data.json"))
+        time_data_pd.to_json(os.path.join(outdir, "time_data.json"))
 
     from post_processing import plot_global_data
     print(time_data_pd)
@@ -460,15 +480,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", required=False,
                         help="JSON configuration string for this experiment")
-    parser.add_argument("--ell", type=float, default=0.1)
-    parser.add_argument("--load_max", type=float, default=2.0)
+    parser.add_argument("--ell", type=float, default=float(1/1.7))
+    parser.add_argument("--load_max", type=float, default=3.0)
     parser.add_argument("--load_min", type=float, default=0.0)
     parser.add_argument("--E", type=float, default=1)
     parser.add_argument("--sigma_D0", type=float, default=1)
     parser.add_argument("--Lx", type=float, default=1)
     parser.add_argument("--Ly", type=float, default=0.1)
     parser.add_argument("--nu", type=float, default=0.0)
-    parser.add_argument("--n", type=int, default=2)
+    parser.add_argument("--n", type=int, default=6)
     parser.add_argument("--nsteps", type=int, default=30)
     parser.add_argument("--degree", type=int, default=2)
     parser.add_argument("--outdir", type=str, default=None)
@@ -476,7 +496,7 @@ if __name__ == "__main__":
     parser.add_argument("--savelag", type=int, default=1)
     parser.add_argument("--parameters", type=str, default=None)
     parser.add_argument("--print", type=bool, default=False)
-    parser.add_argument("--continuation", type=bool, default=False)
+    parser.add_argument("--continuation", type=bool, default=True)
 
     args, unknown = parser.parse_known_args()
     if len(unknown):

src/postprocess.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 import sympy as sp
 import sys, os, sympy, shutil, math
@@ -17,6 +18,11 @@ import os.path
 print('postproc')
 from matplotlib.ticker import ScalarFormatter
 from matplotlib.ticker import FormatStrFormatter
+
+elastic = 'C0'
+homogen = 'C1'
+localis = 'C2'
+unstabl = 'C3'
 
 def load_data(rootdir):
 
@@ -37,6 +43,42 @@ def load_data(rootdir):
 
 	return params, dataf, signature 
 
+def t_stab(ell, q=2):
+	# coeff = 2.*np.pi*q/(q+1)**(3./2.)*np.sqrt(2)
+	coeff_bif = 2*np.pi*np.sqrt(1/6)
+	coeff = coeff_bif/((q+1)/(2.*q))
+	if 1/ell > coeff:
+#     print(1/ell, coeff)
+		return 1.
+	else:
+		return coeff*ell
+
+def t_bif(ell, q=2):
+	# coeff = t_stab(ell, q)*(q+1)/(2.*q)*np.sqrt(2)
+	coeff = 2*np.pi*np.sqrt(1/6)
+	if 1/ell > coeff:
+#     print(1/ell, coeff)
+		return 1.
+	else:
+		return coeff*ell/1
+
+def plot_loadticks(ax, tc, ell):
+	if t_stab(ell)-tc < .1:
+		# label = '$t_c=t_b=t_s$'
+		ax.set_xticks([0,tc])
+		ax.set_xticklabels(['0','$t_c$=$t_b$=$t_s$'])
+	else:
+		ax.set_xticks([0,tc, t_bif(ell), t_stab(ell)])
+		ax.set_xticklabels(['0','$t_c$', '$t_b$', '$t_s$'])
+	return ax
+
+def plot_fills(ax, ell, tc):
+	ax.add_patch(patches.Rectangle((0, 0), 1, 10, facecolor = elastic, fill=True, alpha=.3))
+	ax.add_patch(patches.Rectangle((tc, 0), t_bif(ell)-1, 10, facecolor = homogen, fill=True, alpha=.3))
+	ax.add_patch(patches.Rectangle((t_bif(ell), 0), t_stab(ell)-t_bif(ell), 10, facecolor = 'w', fill=True, alpha=.3))
+	ax.add_patch(patches.Rectangle((t_stab(ell), 0), 10, 10, facecolor = localis, fill=True, alpha=.3))
+	return ax
+
 def plot_spectrum(params, data, tc, ax=None, tol=1e-12):
 	E0 = params['material']['E']
 	w1 = params['material']['sigma_D0']**2/E0
@@ -49,31 +91,40 @@ def plot_spectrum(params, data, tc, ax=None, tol=1e-12):
 					   c=np.where(np.array(d)<tol, 'red', 'C2'))
 					   # c=np.where(np.array(d)<tol, 'C1', 'C2'))
 
-	plt.ylim(-6e-4, 3e-4)
 	plt.axhline(0, c='k', lw=2.)
 	plt.xlabel('$t$')
 	plt.ylabel('Eigenvalues')
 	#     plt.ylabel('$$\\lambda_m$$')
-	plt.axvline(tc, lw=.5, c='k')
+	# plt.axvline(tc, lw=.5, c='k')
 	ax1 = plt.gca()
 	ax1.yaxis.set_major_formatter(FormatStrFormatter('%.0e'))
+
+	plot_loadticks(ax1, tc, ell)
+
 	ax2 = plt.twinx()
 	ax2.plot(data['load'].values, data['alpha_max'].values, label='$$max(\\alpha)$$')
 	ax2.legend()
 	tbif = t_bif(ell)
 	tstab = t_stab(ell)
 	ax2.set_ylabel('max $\\alpha$')
-	ax2.set_ylim(0, 1)
+	ax2.set_ylim(0, 1.03)
+	
 	ax = plt.gca()
-	ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
-	ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
+
+	# ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
+	# ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
 	ax.set_xlim(params['time_stepping']['load_min'], params['time_stepping']['load_max'])
+	
+	ax2.set_yticks([0, 1.])
+	ax2.set_yticklabels(['0','1'])
+	ax2.set_ylim(0, 1.03)
 
-	stable = data['stable'].values
+	# stable = data['stable'].values
+	# ax2.scatter(data['load'].values[stable],  -1.+data['stable'].values[stable], c='k', marker='s', s=70, label='stable')
+	# ax2.scatter(data['load'].values[~stable],     data['stable'].values[~stable], c='red', marker='s', s=70, label='unstable')
 
-	ax2.scatter(data['load'].values[stable],  -1.+data['stable'].values[stable], c='k', marker='s', s=70, label='stable')
-	ax2.scatter(data['load'].values[~stable],     data['stable'].values[~stable], c='red', marker='s', s=70, label='unstable')
-
+	ax1.tick_params(axis='y', labelrotation=90 )
+	plot_fills(ax, ell, tc)
 
 	plt.legend(loc="upper left")
 
@@ -82,12 +133,11 @@ def plot_spectrum(params, data, tc, ax=None, tol=1e-12):
 
 	return fig, ax1, ax2
 
-
 def plot_sigmaeps(params, dataf, tc):
 	E0 = params['material']['E']
 	w1 = params['material']['sigma_D0']**2/E0
 	ell = params['material']['ell']
-	L = params['geometry']['Lx']
+	Lx = params['geometry']['Lx']
 	Ly = params['geometry']['Ly']
 	
 	fig = plt.figure()
@@ -101,20 +151,29 @@ def plot_sigmaeps(params, dataf, tc):
 		# dataf['load'].values*pow(dataf['S(alpha)'].values, -1), marker='o', label='$$\sigma$$')
 
 	plt.plot(dataf['load'].values,
-		dataf['load'].values*dataf['A(alpha)'].values*E0/Ly, marker='o', label='$$\sigma$$')
+		dataf['sigma'].values, marker='o', label='$$\sigma$$', c='k', alpha = 0.7)
+		# dataf['load'].values*dataf['A(alpha)'].values*E0/Ly, marker='o', label='$$\sigma$$')
 
+	sigmaC = params['material']['sigma_D0']
+	ax = plt.gca()
+	ax.set_yticks([0, sigmaC])
+	ax.set_yticklabels(['0','$$\\sigma_c$$'])
+	# ax1.set_yticklabels(['0', '$$10^{-3}$$'])
+	ax.set_xticks([0,tc, t_bif(ell), t_stab(ell)])
+	ax.set_xticklabels(['0','$t_c$', '$t_b$', '$t_s$'])
+	plt.ylim([0, sigmaC*1.1])
+	
 	stable = dataf['stable'].values
 
-
-	ax = plt.gca()
-	ax.axvline(tc, c='k', lw=.5, label='$t^{cr}$')
-	ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
-	ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
+	# ax.axvline(tc, c='k', lw=.5, label='$t^{cr}$')
+	# ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
+	# ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
 	ax.set_xlim(params['time_stepping']['load_min'], params['time_stepping']['load_max'])	
-	plt.scatter(dataf['load'].values[stable], -1.+dataf['stable'].values[stable], c='k', marker='s', s=70, label='stable')
-	plt.scatter(dataf['load'].values[~stable], dataf['stable'].values[~stable], c='red', marker='s', s=70, label='unstable')
 
-	plt.legend(loc="upper left")
+	plot_fills(ax, ell, tc)
+	plot_loadticks(ax, tc, ell)
+
+	plt.legend(loc="upper right")
 
 	return fig, ax
 
@@ -123,44 +182,58 @@ def plot_energy(params, dataf, tc):
 	w1 = params['material']['sigma_D0']**2/E0
 	ell = params['material']['ell']
 	fig = plt.figure()
-
+	Lx = params['geometry']['Lx']
+	Ly = params['geometry']['Ly']
+	En0 = w1 * Lx * Ly
 	t = np.linspace(0., 3., 100)
 	fig = plt.figure()
-	plt.ylabel('Energy')
 	plt.xlabel('$$t$$')
 
 	plt.plot(dataf['load'].values,
-		dataf['dissipated_energy'].values, marker='o', label='dissipated')
+		dataf['dissipated_energy'].values/En0, marker='o', label='dissipated', c='white', alpha = 0.7)
 
 	plt.plot(dataf['load'].values,
-		dataf['elastic_energy'].values, marker='o', lw=2, label='elastic')
+		dataf['elastic_energy'].values/En0, marker='o', label='elastic', c='k', alpha = 0.7)
 
 	plt.plot(dataf['load'].values,
-		(dataf['elastic_energy'].values+dataf['dissipated_energy'].values), marker='o', label='total')
+		(dataf['elastic_energy'].values+dataf['dissipated_energy'].values)/En0, marker='o', lw=3, label='total', c='grey', alpha = 0.7)
+
 	ax = plt.gca()
-	ax.axvline(tc, c='k', lw=.5, label='$t^{cr}$')
-	print(t_stab(ell))
-	print(t_bif(ell))
-	ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label='$t^{cr}_s$')
-	ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
+	# ax.axvline(tc, c='k', lw=.5, label='$t^{cr}$')
+	# print(ell)
+	# print(t_stab(ell))
+	# print(t_bif(ell))
+	# ax.axvline(t_stab(ell), c='k', ls='-', lw=2, label=r'$t^{cr}_s$')
+	# ax.axvline(t_bif(ell), c='k', ls='-.', lw=2, label=r'$t^{cr}_b$')
 	ax.set_xlim(params['time_stepping']['load_min'], params['time_stepping']['load_max'])
-	ax.set_ylim(0, w1*params['geometry']['Lx'])
+	
+	ax.set_ylabel('Energy/$(w_1|\Omega|)$')
+	
+	ax.set_ylim(0, 1.1)
+	ax.set_yticks([0, 1, 1./2.])
+	# ax.set_yticklabels(['$$1$$', '$$1/2$$'])
+
+	plot_loadticks(ax, tc, ell)
+	ax.tick_params(axis='y', labelrotation=90 )
+
+	plot_fills(ax, ell, tc)
 	plt.legend()
 
-	
-	ax.get_yaxis().set_major_formatter(ScalarFormatter())
-	ax.ticklabel_format(axis='both', style='plain', useOffset=True)
+	# ax.add_patch(patches.Rectangle((0, 0), 1, 10, facecolor = elastic, fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((tc, 0), t_bif(ell)-1, 10, facecolor = homogen, fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((t_bif(ell), 0), t_stab(ell)-t_bif(ell), 10, facecolor = localis, fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((1, t_bif(ell)), 10, 10, facecolor = localis,fill=True, alpha=.3))
+
 
 	return fig, ax
 
-
 def plot_stability(prefix, tol=1e-5):
-	import matplotlib.patches as patches
-
 	# dirtree = os.path.join(dirroot, signature)
 	fig = plt.figure()
 	stab_diag = []
 	global_dfs = []
+
+
 	debug = False
 	for subdir, dirs, files in os.walk(prefix):
 		if not os.path.isfile(subdir + "/parameters.pkl"):
@@ -181,12 +254,18 @@ def plot_stability(prefix, tol=1e-5):
 			# print(mineig)
 			# print(np.array(mineig) < tol)
 			loads = df['load'][np.where(np.array(mineig) < tol)[0]].values
-			plt.plot(loads, [1/ell]*len(loads), c='C1', marker='+')
+			plt.plot(loads, [1/ell]*len(loads), c='C0', marker='+')
 			# label='$\\lambda_{min}<\\eta_{tol}$')
 			loads = df['load'][np.where(np.array(mineig) < 0)[0]].values
-			plt.plot(loads, [1/ell]*len(loads), c='C1', marker='X')
+			plt.plot(loads, [1/ell]*len(loads), c='k', marker='X')
 			loads = df['load'][np.where(np.array(mineig) > tol)[0]].values
-			plt.plot(loads, [1/ell]*len(loads), c='C2', marker='.')
+			# plt.plot(loads, [1/ell]*len(loads), c='C2', marker='.')
+			elasticloads = np.where(df['load']<=1)[0]
+
+			# plt.plot(loads, [1/ell]*len(loads), c='C0', marker='.')
+			plt.plot(loads[elasticloads[-1]::], [1/ell]*len(loads[elasticloads[-1]::]), c=homogen, marker='.')
+			plt.plot(loads[elasticloads], [1/ell]*len(loads[elasticloads]), c=elastic, marker='.')
+
 			if debug:
 				print('1/ell, mineog', 1/ell, mineig)
 				print('nonunique loads')
@@ -194,9 +273,11 @@ def plot_stability(prefix, tol=1e-5):
 				print('unstable')
 				print(1/ell, np.where(np.array(mineig) < 0)[0])
 
-	plt.plot((20, 20), (20, 20), ls='-', c='C1', marker='+', label='$\\lambda_0<{}$'.format(tol))
-	plt.plot((20, 20), (20, 20), ls='', c='C1', marker='X', label='incr. unstable')
-	plt.plot((20, 20), (20, 20), ls='', c='C2', marker='.', label='stable, unique')
+
+	# plt.plot((20, 20), (20, 20), ls='-', c='C0', marker='+', label='$\\lambda_0<{}$'.format(tol))
+	plt.plot((20, 20), (20, 20), ls='', c='k', marker='X', label='incr. unstable')
+	plt.plot((20, 20), (20, 20), ls='', c=elastic, marker='.', label='stable, elastic')
+	plt.plot((20, 20), (20, 20), ls='', c=homogen, marker='.', label='stable, homogeneous')
 
 	q=2
 	coeff_sta = 2.*np.pi*q/(q+1)**(3./2.)*np.sqrt(2)
@@ -205,24 +286,27 @@ def plot_stability(prefix, tol=1e-5):
 
 	ax = plt.gca()
 	# ax.plot(loads, [2.*2.*np.pi*q/(q+1)**(3./2.)*np.sqrt(2)/i for i in loads], lw=3, c='k's)
-	ax.plot(loads, [coeff_sta/i for i in loads], '-', c='k', label='$$t_s^{cr}(\ell)$$')
-	ax.plot(loads, [coeff_bif/i for i in loads], '-.', c='k', label='$$t^{cr}_b(\ell)$$')
-	plt.axvline(1.0, c='k')
+	ax.plot(loads, [coeff_sta/i for i in loads], '-', c='k', label='$$t_s(L/\ell)$$')
+	ax.plot(loads, [coeff_bif/i for i in loads], '-.', c='k', label='$$t_b(L/\ell)$$')
+	# plt.axvline(1.0, c='k', lw=1)
 
-	ax.fill_betweenx([coeff_sta/i for i in loads], loads, 20., alpha=.3)
-	ax.fill_betweenx([coeff_bif/i for i in loads], 0, loads, alpha=.3)
-	ax.fill_betweenx([coeff_bif/i for i in loads], 1, loads, alpha=.3, facecolor='C0')
+	ax.fill_betweenx([coeff_sta/i for i in loads], loads, 20., alpha=.3, facecolor=localis)
+	# ax.fill_betweenx([coeff_bif/i for i in loads], 0, loads, alpha=.3, facecolor='C1')
+	ax.fill_betweenx([coeff_bif/i for i in loads], 1, loads, alpha=.3, facecolor=homogen)
 
-	ax.add_patch(patches.Rectangle((0, coeff_bif), 1, 10, facecolor = 'C1',fill=True, alpha=.3))
-	ax.add_patch(patches.Rectangle((1, coeff_sta), 10, 10, facecolor = 'C0',fill=True, alpha=.3))
-	ax.add_patch(patches.Rectangle((0, 0), 10, 1./loads[-2]*coeff_bif, facecolor = 'C1',fill=True, alpha=.3))
-	ax.add_patch(patches.Rectangle((1, 0), loads[-1], coeff_bif/(loads[-1]), facecolor = 'C0',fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((0, coeff_bif), 1, 10, facecolor = 'C0',fill=True, alpha=.3))
+	ax.add_patch(patches.Rectangle((0, 0), 1, 10, facecolor = elastic,fill=True, alpha=.3))
+	ax.add_patch(patches.Rectangle((1, coeff_sta), 10, 10, facecolor = localis,fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((0, 0), 10, 1./loads[-2]*coeff_bif, facecolor = 'C0',fill=True, alpha=.3))
+	# ax.add_patch(patches.Rectangle((1, 0), loads[-1], coeff_bif/(loads[-1]), facecolor = 'C2',fill=True, alpha=.3))
+	x1, y1 = [1, 1], [coeff_bif, 20]
+	# x2, y2 = [1, 10], [3, 2]
+	plt.plot(x1, y1, lw=2, c='k')
 
 	# ax.text(.55, .15, r'Elastic', fontsize=25)
 	# ax.text(1.2, .15, r'Homogeneous (unique)', fontsize=25)
 	# (?# ax.text(3.5, 2.5, r'Unstable', fontsize=25))
 	plt.legend(loc='upper right')
-	
 	plt.xlabel('$t$')
 	plt.ylabel('$$L/\ell$$')
 	plt.ylim(0., 1.5*coeff_sta)
@@ -230,8 +314,8 @@ def plot_stability(prefix, tol=1e-5):
 
 	ax.set_yticks([0, 1, 1/.5, 1/.25, coeff_sta, coeff_bif])
 	ax.set_yticklabels(['0','1','2','4', '$$\\ell_s$$', '$$\\ell_b$$'])
-	
-	visuals.setspines()
+
+	# visuals.setspines()
 	plt.loglog()
 	plt.ylim(0.5, 3*coeff_sta)
 	plt.xlim(0.5, max(loads))
@@ -243,28 +327,7 @@ def load_cont(prefix):
 		dataf = pd.DataFrame(data).sort_values('iteration')
 	return dataf
 
-def t_stab(ell, q=2):
-	coeff = 2.*np.pi*q/(q+1)**(3./2.)*np.sqrt(2)
-	if 1/ell > coeff:
-#     print(1/ell, coeff)
-		return 1.
-	else:
-		return coeff*ell
-
-def t_bif(ell, q=2):
-	coeff = t_stab(ell, q)*(q+1)/(2.*q)*np.sqrt(2)
-	if 1/ell > coeff:
-#     print(1/ell, coeff)
-		return 1.
-	else:
-		return coeff*ell/1
-
-
 def format_params(params):
-	return ('\\ell ={:.2f},'.format(params['material']['ell']),             \
-		'\\nu ={:.0f},'.format(params['material']['nu']),                  \
-		'\\sigma_D0 ={:.0f},'.format(params['material']['sigma_D0']),     \
-		'E ={:.0f}'.format(params['material']['E']))
-
-
-
+    return '$$\ell = {:.2f}, \\nu = {:.1f}, \\sigma_c = {:.1f}, ' \
+           'E = {:.1f}$$'.format(params['material']['ell'], params['material']['nu'],
+                params['material']['sigma_D0'], params['material']['E'])